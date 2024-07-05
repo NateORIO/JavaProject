7a5ff17c02358f35e286f09e7eb9a87a7e0ffdbf
NateORIO <naterx400@gmail.com>
2024-07-05 10:47:32 +0700

    Update eie.java

--- eie.java
+++ eie.java
@@ -1,7 +1,7 @@
 public class eie {
     
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello!!!!!");
         System.out.println("Cat eiei");
         System.out.println("My room have a cat");
     }
