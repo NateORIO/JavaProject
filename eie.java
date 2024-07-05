2a9d7f5345fbbb801bb6ea0dacf140744d0a8efe
NateORIO <naterx400@gmail.com>
2024-07-05 10:47:20 +0700

    This is 21st status

--- eie.java
+++ eie.java
@@ -2,5 +2,7 @@ public class eie {
     
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Cat eiei");
+        System.out.println("My room have a cat");
     }
 }
